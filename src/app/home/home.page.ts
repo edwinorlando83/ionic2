94ab0ba7e408f4733a753833822c6f519aced6d2
Orlando <edwinorlando83@gmail.com>
2020-10-13 20:46:44 -0500

    cast

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,10 +13,14 @@ export class HomePage {
   sueldo=400.55;
   imagen='assets/otros.jpg';
   texto;
+  n1 = '1';
+  n2;
   constructor() {
     this.nombre='Orlando';
   } 
   cambiarNombre(){
     this.nombre = this.texto;
+    let n3 = Number(this.n1);
+    this.n2 = Number(this.n1);
   }
 }
